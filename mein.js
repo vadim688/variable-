621424db156fc16b8b6fded499fcd4553d81f136
Vadim Bulgaru <vadimbulgaru9@gmail.com>
2024-10-07 18:32:16 +0200

    Add files via upload

---
+++ mein.js
@@ -0,0 +1,11 @@
+// Задаем переменные
+let name = "Vadim";  // имя (можно любое)
+let age = 28;       // возраст
+let gender = "man";  // пол (может быть 'man', 'woman' и т.д.)
+let country = "Germany";  // страна
+
+// Проверяем условия
+let result = (age > 28 && name !== "") || (gender !== "man" && country === "Germany");
+
+// Выводим результат
+console.log(result);
